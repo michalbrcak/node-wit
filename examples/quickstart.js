94091d14fe84e66f73cc205c8c6e626b555a47fa
michalbrcak <michal.brcak@gmail.com>
2016-08-27 22:28:05 +0200

    Update quickstart.js

--- examples/quickstart.js
+++ examples/quickstart.js
@@ -13,7 +13,7 @@ try {
 
 const accessToken = (() => {
   if (process.argv.length !== 3) {
-    console.log('usage: node examples/quickstart.js <wit-access-token>');
+    console.log('usage: node examples/quickstart.js KT3BJLFWRMZH6UF3ADAAXL6SK7J735C7');
     process.exit(1);
   }
   return process.argv[2];
