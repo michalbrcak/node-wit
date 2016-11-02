2322b4eac442b856f16134b9f03fb05c216406b3
Michal Brcak <michal.brcak@gmail.com>
2016-11-02 23:59:18 +0100

    Updated QS.js

--- examples/quickstart.js
+++ examples/quickstart.js
@@ -44,18 +44,18 @@ const actions = {
     });
   },
   getForecast({context, entities}) {
-    return new Promise(function(resolve, reject) {
-      var location = firstEntityValue(entities, 'location')
-      if (location) {
-        context.forecast = 'sunny in ' + location; // we should call a weather API here
-        delete context.missingLocation;
-      } else {
-        context.missingLocation = true;
-        delete context.forecast;
-      }
-      return resolve(context);
-    });
-  },
+  return new Promise(function(resolve, reject) {
+    var location = firstEntityValue(entities, "location")
+    if (location) {
+      context.forecast = 'sunny in ' + location; // we should call a weather API here
+      delete context.missingLocation;
+    } else {
+      context.missingLocation = true;
+      delete context.forecast;
+    }
+    return resolve(context);
+  });
+},
 };
 
 const client = new Wit({accessToken, actions});
